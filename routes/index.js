9b1a84dcf2ffb9586d106ea169db26d47a0c8211
Laurent <lo.rail@proton.me>
2024-06-28 11:49:40 +0200

    1 28062024

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+
+router.get('/date', (req, res) => {
+  const date = new Date();
+  const year = date.getFullYear().toString();
+  res.json({ year: year });
+});
+
+
+
+
+module.exports = router;
